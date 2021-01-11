30b825db9cbd6a90854d4fe6ee3b04002244bc4a
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-11 20:07:55 +0000

    OddEven

---
+++ Day3/main.py
@@ -0,0 +1,21 @@
+# print("Welcome to the roller coaster ride.")
+
+# height = int(input("What is your height?"))
+
+# if height >= 120:
+#     print("Yes you can ride.")
+# else:
+#     print("No you are NOT tall enough.")
+
+
+############## ODD or EVEN #####################
+
+
+print("Welcome to Odd or Even Checker")
+
+number = int(input("What number would you like to check? "))
+
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an Odd number")
