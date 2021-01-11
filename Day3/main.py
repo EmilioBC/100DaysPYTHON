cb4aa24fc80684f625f762da73da2eb9933cdd05
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-11 21:50:10 +0000

    bmi

--- Day3/main.py
+++ Day3/main.py
@@ -23,18 +23,43 @@
 
 ################3 Nested If Condition ################
 
-print("Welcome to the roller coaster ride.")
+# print("Welcome to the roller coaster ride.")
 
-height = int(input("What is your height?"))
+# height = int(input("What is your height?"))
 
-if height >= 120:
-    print("Yes you can ride.")
-    age = int(input("How old are you? "))
-    if age < 12:
-        print("Please pay $5")
-    elif age >= 12 or age <= 18:
-        print("Please pay $7")
-    else:
-        print("Please pay $12")
+# if height >= 120:
+#     print("Yes you can ride.")
+#     age = int(input("How old are you? "))
+#     if age < 12:
+#         print("Please pay $5")
+#     elif age >= 12 or age <= 18:
+#         print("Please pay $7")
+#     else:
+#         print("Please pay $12")
+# else:
+#     print("No you are NOT tall enough.")
+
+
+############# BMI CALCULATOR #######################
+
+print("BMI Calculator")
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+bmi = round(weight / (height * height))
+
+if bmi < 18.5:
+    print(f"BMI:{bmi} Underweight")
+elif bmi >= 18.5 or bmi <= 25:
+    print(f"BMI:{bmi} Normal Weight")
+elif bmi > 25 or bmi <= 30:
+    print(f"BMI:{bmi} Slightly overweight")
+elif bmi > 30 or bmi <= 35:
+    print(f"BMI: {bmi} Obese")
 else:
-    print("No you are NOT tall enough.")
+    print(f"BMI:{bmi} Clinically Obese")
