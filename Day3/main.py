7b28f5b98a3342aac7e78e07c6741b6c2362cad1
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-11 20:21:28 +0000

    nestedIf

--- Day3/main.py
+++ Day3/main.py
@@ -11,11 +11,30 @@
 ############## ODD or EVEN #####################
 
 
-print("Welcome to Odd or Even Checker")
+# print("Welcome to Odd or Even Checker")
 
-number = int(input("What number would you like to check? "))
+# number = int(input("What number would you like to check? "))
 
-if number % 2 == 0:
-    print(f"{number} is an even number.")
+# if number % 2 == 0:
+#     print(f"{number} is an even number.")
+# else:
+#     print(f"{number} is an Odd number")
+
+
+################3 Nested If Condition ################
+
+print("Welcome to the roller coaster ride.")
+
+height = int(input("What is your height?"))
+
+if height >= 120:
+    print("Yes you can ride.")
+    age = int(input("How old are you? "))
+    if age < 12:
+        print("Please pay $5")
+    elif age >= 12 or age <= 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
 else:
-    print(f"{number} is an Odd number")
+    print("No you are NOT tall enough.")
